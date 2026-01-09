fc0d5b3c05395a0e4e7f7af065b91ea7e5770cd0
natythaiz <101813116+natythaiz@users.noreply.github.com>
2026-01-09 11:00:40 -0300

    feat: implement class BookDAO, LoanDAO and UserDAO

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -10,30 +10,7 @@ import entities.enumeradores.Status;
 public class Program {
 
 	public static void main(String[] args) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = null;
-        
-        try {
-            tx = session.beginTransaction(); // ABRIR TRANSAÇÃO É OBRIGATÓRIO PARA SALVAR
-
-            // Criando um dado de teste
-            Book livro = new Book();
-            livro.setTitulo("O Pequeno Principe");
-            livro.setAutor("Antoine de Saint-Exupéry");
-            livro.setIsbn(10);
-            livro.setStatus(Status.DISPONIVEL);
-
-            session.save(livro); 
-
-            tx.commit(); 
-            System.out.println("Tabela criada e Livro salvo com sucesso!");
-
-        } catch (Exception e) {
-            if (tx != null) tx.rollback(); 
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+		
 	}
 
 }
